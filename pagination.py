ef0505f9ece516f51a98dc2873062ff0f14502f6
hal <bowtiedai@gmail.com>
2022-03-27 17:51:59 -0400

    small change

--- pagination.py
+++ pagination.py
@@ -2,35 +2,6 @@ import pickle as pkl
 import json
 import pdb
 
-"""
-<div class="container mx-auto w-1/2 p-4 relative">
-  <img src="https://storage.googleapis.com/sentinel-nft/raw-assets/c_0x8a90cab2b38dba80c64b7734e58ee1db38b8992e_t_9999_raw_asset.png" alt="" class="w-full h-auto">
-  <div class="bg-gradient-to-r opacity-95 from-yellow-200 to-yellow-500 w-24 h-1/8 absolute top-8 left-6 rounded-lg p-2.5 text-center text-yellow-900 font-bold">Top 43%</div>
-</div>
-
-
-"""
-
-"""
-  <p class="text-blue-600 hover:text-blue-800 text-center mb-4 text-xl">
-  <a href="#1" target="_blank">1</a> &emsp; <a href="#1" target="_blank">2</a> &emsp; <a href="#1" target="_blank">3</a> &emsp; ... &emsp; <a href="#1" target="_blank">10</a>
-  </p>
-"""
-
-"""
-<div class="flex justify-center items-center h-screen mb-4">
-  <button class="bg-transparent hover:bg-blue-500 text-blue-600 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded text-center">
-    Prev
-  </button>
-  &emsp;&emsp;
-  <button class="bg-transparent hover:bg-blue-500 text-blue-600 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded text-center">
-    Next
-  </button>
-</div>
-
-</div>
-"""
-
 def create_html(json_data_python):
     html = f"""
     <!DOCTYPE html>
@@ -69,20 +40,6 @@ def create_html(json_data_python):
 
             function nextPage() {{
                 page = page + 1;
-                // delete the existing my gallery and next/prev links
-                var el_p = document.getElementById("buttonsForChangePage");
-                el_p.remove();
-
-                var el = document.getElementById("actualMyGalleryDiv");
-                el.remove();
-
-                console.log("new page number " + page);
-                onLoad();
-                scroll(0,0);
-            }}
-
-            function prevPage() {{
-                page = page - 1;
                 // delete the existing my gallery and next/prev links
                 var el_p = document.getElementById("buttonsForChangePage");
                 el_p.remove();
@@ -175,34 +132,14 @@ def create_html(json_data_python):
                 buttons_div.id = "buttonsForChangePage";
                 buttons_div.className = "flex justify-center items-center h-screen mb-4";
 
-                if (page == 0) {{
-                    var next_button_div = document.createElement("button");
-                    next_button_div.className = "bg-blue-400 hover:bg-blue-600 text-white text-lg font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded text-center";
-                    next_button_div.innerHTML = "Next";
-                    next_button_div.addEventListener('click', function(){{
-                        nextPage();
-                    }});
-                    buttons_div.appendChild(next_button_div)
 
-                }} else {{
-                    var prev_button_div = document.createElement("button");
-                    prev_button_div.className = "bg-blue-400 hover:bg-blue-600 text-white text-lg font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded text-center";
-                    prev_button_div.innerHTML = "Prev";
-                    prev_button_div.addEventListener('click', function(){{
-                        prevPage();
-                    }});
-                    buttons_div.appendChild(prev_button_div)
-
-                    buttons_div.innerHTML += "&emsp;";
-
-                    //var next_button_div = document.createElement("button");
-                    //next_button_div.className = "bg-blue-400 hover:bg-blue-600 text-white text-lg font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded text-center";
-                    //next_button_div.innerHTML = "Next";
-                    //next_button_div.addEventListener('click', function(){{
-                    //    nextPage();
-                    //}});
-                    //buttons_div.appendChild(next_button_div)
-                }}
+                var next_button_div = document.createElement("button");
+                next_button_div.className = "bg-blue-400 hover:bg-blue-600 text-white text-lg font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded text-center";
+                next_button_div.innerHTML = "Next";
+                next_button_div.addEventListener('click', function(){{
+                    nextPage();
+                }});
+                buttons_div.appendChild(next_button_div)
 
                 main_div.appendChild(buttons_div);
                 console.log("done");
